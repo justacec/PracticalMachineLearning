a6ac981acd68709f7c140228e710bfd4c6c2035b
Justace Clutter <justacec@gmail.com>
2014-06-20 07:55:13 -0400

    Added needed code for question 2.. have not run..

--- quiz_4.R
+++ quiz_4.R
@@ -43,3 +43,43 @@ comb_accuracy = sum(pred_comb == agreeSub$y) / length(pred_comb)
 #   GBM Accuracy = 0.5260
 #   Agreement Accuracy = 0.6389
 
+
+# Question 2 --------------------------------------------------------------
+
+rm(list = ls())
+set.seed(3433)
+library(AppliedPredictiveModeling)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+set.seed(62433)
+model_rf = train(diagnosis ~ ., method = 'rf', data = training)
+model_gbm = train(diagnosis ~ ., method = 'gbm', data = training)
+model_lda = train(diagnosis ~ ., method = 'lda', data = training)
+
+pred_rf = predict(model_rf, training)
+pred_gbm = predict(model_gbm, training)
+pred_lda = predict(model_lda, training)
+
+comb_data = data.frame(rf = pred_rf, gbm = pred_gbm, lda = pred_lda, diagnosis = training$diagnosis)
+model_comb = train(diagnosis ~ ., method = 'rf', data = comb_data)
+
+pred_rf_test = predict(model_rf, testing)
+pred_gbm_test = predict(model_gbm, testing)
+pred_lda_test = predict(model_lda, testing)
+comb_data_test = data.frame(rf = pred_rf_test, gbm = pred_gbm_test, lda = pred_lda_test, diagnosis = testing$diagnosis)
+pred_comb_test = predict(model_comb, comb_data_test)
+
+accuracy_rf = sum(pred_rf_test == testing$diagnosis) / length(pred_rf_test)
+accuracy_gbm = sum(pred_gbm_test == testing$diagnosis) / length(pred_gbm_test)
+accuracy_lda = sum(pred_lda_test == testing$diagnosis) / length(pred_lda_test)
+
+accuracy_comb = sum(pred_comb_test == comb_data_test$diagnosis) / length(pred_comb_test)
+
+
+pred_comb = predict(model_comb, comb_data)
+
+# Need to make sure to generate the 
